dd17a5544bd07049387d1727184bc18b0a302851
quest4ione <quest4ione@gmail.com>
2026-04-23 17:21:16 +0200

    generalize ToIndex

--- qelbus/src/memory/array.rs
+++ qelbus/src/memory/array.rs
@@ -1,53 +1,6 @@
 use nostd_cow::RefCow;
 
-use super::Memory;
-
-pub trait ToIndex: Sized {
-    fn to_index(address: RefCow<'_, Self>) -> Option<usize>;
-}
-
-macro_rules! impl_toindex_prim {
-    { $($signed: ty as $unsigned: ty),* $(,)? } => {
-        $(
-        impl ToIndex for $unsigned {
-            fn to_index(address: RefCow<'_, Self>) -> Option<usize> {
-                address.into_owned().try_into().ok()
-            }
-        }
-
-        impl ToIndex for $signed {
-            fn to_index(address: RefCow<'_, Self>) -> Option<usize> {
-                (address.into_owned() as $unsigned).try_into().ok()
-            }
-        }
-        )*
-    }
-}
-
-impl_toindex_prim! {
-    i8 as u8,
-    i16 as u16,
-    i32 as u32,
-    i64 as u64,
-    i128 as u128,
-    isize as usize,
-}
-
-#[cfg(feature = "num-bigint")]
-impl ToIndex for num_bigint::BigUint {
-    fn to_index(address: RefCow<'_, Self>) -> Option<usize> {
-        (&*address).try_into().ok()
-    }
-}
-
-#[cfg(feature = "num-bigint")]
-impl ToIndex for num_bigint::BigInt {
-    fn to_index(address: RefCow<'_, Self>) -> Option<usize> {
-        (&*address).try_into().ok()
-    }
-}
-
-pub struct AddressOutOfRangeError;
+use super::{IndexOutOfRangeError, Memory, ToIndex, to_index};
 
 pub struct ArrayMemory<const N: usize, T>([T; N]);
 
@@ -55,21 +8,21 @@ impl<const N: usize, T> Memory<T> for ArrayMemory<N, T>
 where
     T: Clone + ToIndex,
 {
-    type Error = AddressOutOfRangeError;
+    type Error = IndexOutOfRangeError<T>;
 
     fn get(&self, address: RefCow<'_, T>) -> Result<RefCow<'_, T>, Self::Error> {
-        let address = ToIndex::to_index(address).ok_or(AddressOutOfRangeError)?;
+        let index = to_index(&*address)?;
         self.0
-            .get(address)
+            .get(index)
             .map(RefCow::Borrowed)
-            .ok_or(AddressOutOfRangeError)
+            .ok_or_else(|| IndexOutOfRangeError::from(address.into_owned()))
     }
 
     fn set(&mut self, address: RefCow<'_, T>, value: RefCow<'_, T>) -> Result<(), Self::Error> {
-        let address = ToIndex::to_index(address).ok_or(AddressOutOfRangeError)?;
+        let index = to_index(&*address)?;
         self.0
-            .get_mut(address)
+            .get_mut(index)
             .map(|old| *old = value.into_owned())
-            .ok_or(AddressOutOfRangeError)
+            .ok_or_else(|| IndexOutOfRangeError::from(address.into_owned()))
     }
 }
--- qelbus/src/memory/mod.rs
+++ qelbus/src/memory/mod.rs
@@ -1,8 +1,13 @@
 mod array;
 
-pub use self::array::{AddressOutOfRangeError, ArrayMemory, ToIndex};
+use core::{
+    error::Error,
+    fmt::{Debug, Display},
+};
 
 use nostd_cow::RefCow;
+
+pub use self::array::ArrayMemory;
 
 pub trait Memory<T> {
     type Error;
@@ -10,4 +15,81 @@ pub trait Memory<T> {
     fn get(&self, address: RefCow<'_, T>) -> Result<RefCow<'_, T>, Self::Error>;
 
     fn set(&mut self, address: RefCow<'_, T>, value: RefCow<'_, T>) -> Result<(), Self::Error>;
+}
+
+pub trait ToIndex: Sized {
+    fn to_index(&self) -> Option<usize>;
+}
+
+macro_rules! impl_toindex_prim {
+    { $($signed: ty as $unsigned: ty),* $(,)? } => {
+        $(
+        impl ToIndex for $unsigned {
+            fn to_index(&self) -> Option<usize> {
+                (*self).try_into().ok()
+            }
+        }
+
+        impl ToIndex for $signed {
+            fn to_index(&self) -> Option<usize> {
+                (*self as $unsigned).try_into().ok()
+            }
+        }
+        )*
+    }
+}
+
+impl_toindex_prim! {
+    i8 as u8,
+    i16 as u16,
+    i32 as u32,
+    i64 as u64,
+    i128 as u128,
+    isize as usize,
+}
+
+#[cfg(feature = "num-bigint")]
+impl ToIndex for num_bigint::BigUint {
+    fn to_index(&self) -> Option<usize> {
+        self.try_into().ok()
+    }
+}
+
+#[cfg(feature = "num-bigint")]
+impl ToIndex for num_bigint::BigInt {
+    fn to_index(&self) -> Option<usize> {
+        self.try_into().ok()
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct IndexOutOfRangeError<T>(T);
+
+impl<T> From<T> for IndexOutOfRangeError<T> {
+    fn from(value: T) -> Self {
+        IndexOutOfRangeError(value)
+    }
+}
+
+impl<T: Display> Display for IndexOutOfRangeError<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "index {} out of range", self.0)
+    }
+}
+
+impl<T: Display + Debug> Error for IndexOutOfRangeError<T> {}
+
+impl<T> IndexOutOfRangeError<T> {
+    pub fn index(&self) -> &T {
+        &self.0
+    }
+
+    pub fn into_index(self) -> T {
+        self.0
+    }
+}
+
+pub fn to_index<T: ToIndex + Clone>(value: &T) -> Result<usize, IndexOutOfRangeError<T>> {
+    value.to_index().ok_or_else(|| value.clone().into())
 }
